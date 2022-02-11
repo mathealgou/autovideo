e8d51c62afae8af81c6b55bcd55776fca5e4d4cd
Matheus <mathealgou@gmail.com>
2022-02-11 19:12:06 -0300

    Command line functionality is now fully covered by the terminal file.

--- main.py
+++ main.py
@@ -1,28 +1,6 @@
-import os
-import sys
-from scraper import scrape
-from scraper import search
-from terminal import *
+from terminal import * 
 
 # A little introduction
 introduction("Autovideo", "1.0")
 
-# for testing's sake
-#print(search("bacon"))
-print("Enter a search term:")
-term = input()
-print(f"Searching for {term}...")
-article = search(term)
-print(article[1])
-print("Is this the article you were looking for?")
-answer = input()
-if answer.lower() == "y":
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Great!")
-    #Procced to do the video
-elif answer.lower() == "n":
-    #Clear the terminal and restart the program
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("Please try again.")
-    os.execl(sys.executable, sys.executable, *sys.argv)
-
+article = search_article()
--- terminal.py
+++ terminal.py
@@ -1,3 +1,7 @@
+from scraper import *
+import os
+import sys
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -11,3 +15,22 @@ class bcolors:
 
 def introduction(name, version, creator="Matheus Goulart"):
     print(f"{bcolors.OKGREEN}Autovideo {version} by {creator}{bcolors.ENDC}")
+
+def search_article():
+    print("Enter a search term:")
+    term = input()
+    print(f"Searching for {term}...")
+    article = search(term)
+    print(article[1])
+    print("Is this the article you were looking for?")
+    answer = input()
+    if answer.lower() == "y":
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Great!")
+        #Procced to do the video
+        return article
+    elif answer.lower() == "n":
+        #Clear the terminal and restart the program
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print("Please try again.")
+        os.execl(sys.executable, sys.executable, *sys.argv)
