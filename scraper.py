ca6d79078f529f1f6d1f3a658d9226ec938fbbf1
Matheus <mathealgou@gmail.com>
2022-02-11 00:50:07 -0300

    Cleaner results from the scraper.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def scrape(url):
     response = requests.get(url)
@@ -8,7 +9,28 @@ def scrape(url):
     
     #result = soup.prettify()
 
-    result = soup.find_all("p")
-    result = result.prettify()
+    ps = soup.find_all("p")
+    result = []
+    for p in ps:
+        result.append(p.text)
+    
+    result = clean_text(result)
 
     return result
+
+def clean_text(text):
+    if type(text) is list:
+        # DEBUG LINE
+        print("is list")
+        result = []
+        for t in text:
+            t = t.replace("\n", "")
+            result.append(re.sub("[\[].*?[\]]", "", t))
+        return result
+    elif type(text) is str:
+        # DEBUG LINE
+        print("is not list")
+        result = re.sub("[\(\[].*?[\)\]]", "", t)
+        return result
+    else:
+        return f"Error: clean_text() cannot clean data of type{type(text)}"
