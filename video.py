3606a0a6bc9c455ddfacda7b2a0bb209324e5107
Matheus <mathealgou@gmail.com>
2022-02-12 13:20:07 -0300

    Cleaner code.

--- video.py
+++ video.py
@@ -2,22 +2,24 @@ import cv2 as cv
 import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 
-width = 1280
-height = 720
-FPS = 24
-seconds = 20
-
-fourcc = VideoWriter_fourcc(*'XVID')
-video = VideoWriter('./noise.avi', fourcc, float(FPS), (width, height))
-
-def render():
+# This is the function that will be called by the main file.
+def render(width, height, FPS, seconds, filename):
+    # Define the codec and create VideoWriter object
+    fourcc = VideoWriter_fourcc(*'XVID')
+    video = VideoWriter(f"./{filename}.avi", fourcc, float(FPS), (width, height))
+    
+    # For every frame of video
     for _ in range(FPS*seconds):
+
+        # Create a black background, which serves as the canvas.
         frame = np.zeros((height, width, 3), np.uint8)
-        # frame = np.random.randint(0, 20, 
-        #                           (height, width, 3), 
-        #                           dtype=np.uint8)
-        cv.putText(frame, "Press q to quit", (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        # Do stuff with the video (text, images, etc.)
+        cv.putText(frame, "Test", (640, 360), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+        # Write the frame into the file
         video.write(frame)
+    #Once the loop is done, release the video writer and close the file.
     video.release()
 
-render()
+render(1280, 720, 24, 20, "test")
