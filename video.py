b44949c4ef7dc8aa8e58e5c7cc703c9721c3e819
Matheus <mathealgou@gmail.com>
2022-02-12 13:08:29 -0300

    Video renderer now creates a black background.

--- video.py
+++ video.py
@@ -1,18 +1,20 @@
-import cv2
+import cv2 as cv
 import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 
 width = 1280
 height = 720
 FPS = 24
-seconds = 10
+seconds = 20
 
-fourcc = VideoWriter_fourcc(*'MP42')
+fourcc = VideoWriter_fourcc(*'XVID')
 video = VideoWriter('./noise.avi', fourcc, float(FPS), (width, height))
 
 for _ in range(FPS*seconds):
-    frame = np.random.randint(0, 256, 
-                              (height, width, 3), 
-                              dtype=np.uint8)
+    frame = np.zeros((height, width, 3), np.uint8)
+    # frame = np.random.randint(0, 20, 
+    #                           (height, width, 3), 
+    #                           dtype=np.uint8)
+    cv.putText(frame, "Press q to quit", (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     video.write(frame)
 video.release()
