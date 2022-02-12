c15773a50a914af994837303d7f09f1c0e82f514
Matheus <mathealgou@gmail.com>
2022-02-12 13:11:44 -0300

    Render code extracted into a function.

--- video.py
+++ video.py
@@ -10,11 +10,14 @@ seconds = 20
 fourcc = VideoWriter_fourcc(*'XVID')
 video = VideoWriter('./noise.avi', fourcc, float(FPS), (width, height))
 
-for _ in range(FPS*seconds):
-    frame = np.zeros((height, width, 3), np.uint8)
-    # frame = np.random.randint(0, 20, 
-    #                           (height, width, 3), 
-    #                           dtype=np.uint8)
-    cv.putText(frame, "Press q to quit", (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-    video.write(frame)
-video.release()
+def render():
+    for _ in range(FPS*seconds):
+        frame = np.zeros((height, width, 3), np.uint8)
+        # frame = np.random.randint(0, 20, 
+        #                           (height, width, 3), 
+        #                           dtype=np.uint8)
+        cv.putText(frame, "Press q to quit", (10, 20), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        video.write(frame)
+    video.release()
+
+render()
