761bfe441cb0eab227b2377e546a1f6048a3097a
Matheus <mathealgou@gmail.com>
2022-02-13 10:15:20 -0300

    Video.render() now has some security measures.

--- video.py
+++ video.py
@@ -1,12 +1,41 @@
+import terminal as terminal
+from typing import List
 import cv2 as cv
 import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 
 # This is the function that will be called by the main file.
-def render(width, height, FPS, seconds, filename):
+def render(width, height, FPS, seconds, filename, article):
+
+    # Security measures
+    if type(width) is not int:
+        terminal.error("width must be an integer")
+        return "Error: width must be an integer"
+    if type(height) is not int:
+        terminal.error("height must be an integer")
+        return "Error: height must be an integer"
+    if type(FPS) is not int:
+        terminal.error("FPS must be an integer")
+        return "Error: FPS must be an integer"
+    if type(seconds) is not int:
+        terminal.error("seconds must be an integer")
+        return "Error: seconds must be an integer"
+    if type(filename) is not str:
+        terminal.error("filename must be a string")
+        return "Error: filename must be a string"
+    if type(article) is not list:
+        terminal.error("article must be a list")
+        return "Error: article must be a list"
+    if type(article[0]) is not str:
+        terminal.error("article must be a list of strings")
+        return "Error: article must be a list of strings"
+    # End of security measures
+
     # Define the codec and create VideoWriter object
     fourcc = VideoWriter_fourcc(*'XVID')
     video = VideoWriter(f"./{filename}.avi", fourcc, float(FPS), (width, height))
+
+
     
     # For every frame of video
     for _ in range(FPS*seconds):
@@ -15,11 +44,12 @@ def render(width, height, FPS, seconds, filename):
         frame = np.zeros((height, width, 3), np.uint8)
 
         # Do stuff with the video (text, images, etc.)
-        cv.putText(frame, "Test", (640, 360), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+        cv.putText(frame, "Test", (640, 360), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2)
 
         # Write the frame into the file
         video.write(frame)
     #Once the loop is done, release the video writer and close the file.
     video.release()
 
-#render(1280, 720, 24, 20, "test")
+    
+render(1280, 720, 24, 20, "test", ["test", "test2"])
