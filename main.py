9171a891e9536f431393e8bc7f2fbcf80cf5d226
Matheus <mathealgou@gmail.com>
2022-02-18 22:12:10 -0300

    Once again confident in calling the render function.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+from ast import parse
 import terminal as terminal
 import video as video
+from article_parser import parse_article
 
 # A little introduction
 terminal.introduction("Autovideo", "1.0")
@@ -7,7 +9,12 @@ terminal.introduction("Autovideo", "1.0")
 article = terminal.search_article()
 
 terminal.debug("Rendering video...")
-video.render(720, 1280, 24, 20, "bacon", article)
+
+parsed_article = parse_article(article)
+
+print(parsed_article)
+
+video.render(720, 1280, 24, 20, "bacon", parsed_article)
 
 
 terminal.statement("Done!")
