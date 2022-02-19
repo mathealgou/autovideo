372bf19dd44414908c0ebfcf5e2f40a7c9b91ee6
Matheus <mathealgou@gmail.com>
2022-02-18 22:11:45 -0300

    Video can now render multiple lines.

--- video.py
+++ video.py
@@ -1,55 +1,49 @@
-import terminal as terminal
 from typing import List
+import terminal as terminal
 import cv2 as cv
 import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 
-# This is the function that will be called by the main file.
-def render(width, height, FPS, seconds, filename, article):
 
-    # Security measures
-    if type(width) is not int:
-        terminal.error("width must be an integer")
-        return "Error: width must be an integer"
-    if type(height) is not int:
-        terminal.error("height must be an integer")
-        return "Error: height must be an integer"
-    if type(FPS) is not int:
-        terminal.error("FPS must be an integer")
-        return "Error: FPS must be an integer"
-    if type(seconds) is not int:
-        terminal.error("seconds must be an integer")
-        return "Error: seconds must be an integer"
-    if type(filename) is not str:
-        terminal.error("filename must be a string")
-        return "Error: filename must be a string"
-    if type(article) is not list:
-        terminal.error("article must be a list")
-        return "Error: article must be a list"
-    if type(article[0]) is not str:
-        terminal.error("article must be a list of strings")
-        return "Error: article must be a list of strings"
-    # End of security measures
+
+# This is the function that will be called by the main file.
+def render(width: int, height: int, FPS: int, seconds: float, filename: str, article: List[dict]):
 
     # Define the codec and create VideoWriter object
     fourcc = VideoWriter_fourcc(*'XVID')
     video = VideoWriter(f"./{filename}.avi", fourcc, float(FPS), (width, height))
 
+    print(article)
 
-    
-    # For every frame of video
-    for _ in range(FPS*seconds):
+    for paragraph in article:
+        text = paragraph["text"]
 
-        # Create a black background, which serves as the canvas.
-        frame = np.zeros((height, width, 3), np.uint8)
+        text_with_newlines = '\n'.join(text[i:i+30] for i in range(0, len(text), 30))
 
-        # Do stuff with the video (text, images, etc.)
-        cv.putText(frame, "Test", (640, 360), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2)
+        duration = int(paragraph["duration"])
 
-        # Write the frame into the file
-        video.write(frame)
+
+        print(text_with_newlines)
+        # For every frame of video
+        for _ in range(FPS*duration):
+
+            # Create a black background, which serves as the canvas.
+            frame = np.zeros((height, width, 3), np.uint8)
+
+            # Do stuff with the video (text, images, etc.)
+            # cv.putText(frame, text, (0, int(height / 2)), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2)
+
+            y0, dy = 50, 20
+            for i, line in enumerate(text_with_newlines.split('\n')):
+                print(line)
+                y = y0 + i*dy
+                cv.putText(frame, line, (0, y), cv.FONT_HERSHEY_SIMPLEX, 1, (200, 200, 200), 2)
+
+            # Write the frame into the file
+            video.write(frame)
+
     #Once the loop is done, release the video writer and close the file.
     video.release()
 
     
-render(1280, 720, 24, 20, "test", ["test", "test2"])
+# render(1280, 720, 24, 20, "test", ["test", "test2"])
