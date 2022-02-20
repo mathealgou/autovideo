e3fc0d31308d8fbad2942419edc66df9d18364a5
Matheus <mathealgou@gmail.com>
2022-02-20 20:32:54 -0300

    Video.py now indicates how many frames vave been rendered during render.

--- video.py
+++ video.py
@@ -10,12 +10,17 @@ from cv2 import VideoWriter, VideoWriter_fourcc
 # This is the function that will be called by the main file.
 def render(width: int, height: int, FPS: int, seconds: float, filename: str, article: List[dict]):
 
-    terminal.clear()
-    terminal.statement("Rendering video...")
 
     # Define the codec and create VideoWriter object
     fourcc = VideoWriter_fourcc(*'XVID')
     video = VideoWriter(f"./{filename}.avi", fourcc, float(FPS), (width, height))
+
+    # calculate the total number of frames to be rendered
+    total_frames = 0
+    rendered_frames = 0
+    for paragraph in article:
+        total_frames += int(paragraph["duration"] * FPS)
+    
 
     for paragraph in article:
         text = paragraph["text"]
@@ -24,10 +29,13 @@ def render(width: int, height: int, FPS: int, seconds: float, filename: str, art
         # TODO - Move this to the article parser
         text_with_newlines = textwrap.wrap(text, width=30)
 
-        duration = int(paragraph["duration"])
+        duration = int(paragraph["duration"])        
 
         # For every frame of video
         for _ in range(FPS*duration):
+            terminal.clear()
+            terminal.statement("Rendering video...")
+            terminal.statement(f"{rendered_frames}/{total_frames} frames rendered")
 
             # Create a black background, which serves as the canvas.
             frame = np.zeros((height, width, 3), np.uint8)
@@ -45,6 +53,8 @@ def render(width: int, height: int, FPS: int, seconds: float, filename: str, art
 
             # Write the frame into the file
             video.write(frame)
+
+            rendered_frames += 1
 
     #Once the loop is done, release the video writer and close the file.
     video.release()
