a31778f84861c84db802e11001d8e8dcd323f1af
Matheus <mathealgou@gmail.com>
2022-03-01 21:07:24 -0300

    Now there's an indication that the images are being downloaded.

--- main.py
+++ main.py
@@ -22,7 +22,11 @@ terminal.ask("Enter a filename:")
 
 filename = input()
 
+terminal.statement("Downloading images...")
+
 scraper.get_images(term, filename)
+
+terminal.clear()
 
 video.render(720, 1280, 1.0, 20, filename, parsed_article)
 
